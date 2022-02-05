e8658b18c6038e0b5958811d27c72f79dd071d72
cwcraw <cwcraw@gmail.com>
2022-02-05 14:55:26 +0900

    Working on Deployment

--- WavePage/settings.py
+++ WavePage/settings.py
@@ -61,12 +61,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "WavePage.wsgi.application"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
