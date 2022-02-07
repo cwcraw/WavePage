72c4eba2bd6b2794a67e8f6c6207fb1f2fe256be
cwcraw <cwcraw@gmail.com>
2022-02-07 09:16:58 +0900

    Trying to deploy

--- WavePage/settings.py
+++ WavePage/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["*wavepage.herokuapp.*", "127.0.0.1"]
+ALLOWED_HOSTS = ["*wavepage.herokuapp.*", "127.0.0.1", "*"]
 
 
 # Application definition
