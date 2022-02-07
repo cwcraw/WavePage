d1f0953fd98a2be13852d14966a36d0ab4be12a8
cwcraw <cwcraw@gmail.com>
2022-02-07 10:24:03 +0900

    Configuring whitenoise

--- WavePage/wsgi.py
+++ WavePage/wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WavePage.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root="/static")
+application = WhiteNoise(application, root="static")
